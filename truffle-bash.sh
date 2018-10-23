a490820132db6544f14a082c12625490296bffb8
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2018-10-23 12:30:11 +0500

    fix: added npm run truffle-bash cmd

---
+++ truffle-bash.sh
@@ -0,0 +1,14 @@
+echo "MAP - truffle script..."
+echo "deleting build directory..."
+rm -r build
+echo "DONE!"
+echo "deleting app/contracts"
+rm -r app/contracts
+echo "DONE!"
+echo "Compile and Migrate contracts..."
+truffle migrate --compile-all --reset --network ganache
+echo "DONE!"
+echo "Copying contracts to app..."
+cp -r -f build/contracts app
+echo "DONE!"
+sleep 5
